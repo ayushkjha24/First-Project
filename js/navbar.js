707b26844cc9c119b85ef6ae7e0211bbf3345d8c
rajnishrj <122149956+rajnishrj@users.noreply.github.com>
2024-02-03 08:43:29 +0530

    navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,1 +1,18 @@
+        document.addEventListener('DOMContentLoaded', function () {
+            const mobileMenu = document.getElementById('mobile-menu');
+            const navList = document.querySelector('.nav-list');
+
+            mobileMenu.addEventListener('click', function () {
+                navList.classList.toggle('show');
+                toggleBars();
+            });
+
+            function toggleBars() {
+                const bars = document.querySelectorAll('.menu-toggle span');
+
+                bars.forEach(bar => {
+                    bar.classList.toggle('cross');
+                });
+            }
+        });
 
